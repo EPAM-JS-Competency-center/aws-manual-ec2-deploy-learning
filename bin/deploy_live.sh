17673c548c1502d23cad1571bab2b5c6ac35892e
ruslan <ruslanusd@gmail.com>
2021-10-04 17:20:13 +0300

    simplify deploy script for nodejs app

--- bin/deploy_live.sh
+++ bin/deploy_live.sh
@@ -2,16 +2,10 @@
 
 #configure paths
 branch="master"
-branch_dir="/var/www/node-app-deploy"
-deploy_dir="$branch_dir/deploy"
-all_versions_dir="$branch_dir/versions"
-code_link="$branch_dir/code"
-previous_links="$branch_dir/previous_links/"
-deploy_date=$(date -u +%Y_%m_%d_T%H_%M_%S)
-version_dir="$all_versions_dir/$deploy_date"
+app_dir="/var/www/node-app-deploy"
 
 #pull from git
-cd $deploy_dir
+cd $app_dir
 git fetch --all
 git reset --hard origin/$branch
 git pull origin $branch
@@ -23,16 +17,7 @@ npm install
 #npm run test
 #migrate database
 
-#deploy
-mkdir $version_dir
-rsync -a  --exclude=".git/" --exclude="src/"  $deploy_dir/ $version_dir/
-
-#switch to a new version
-cp -Pf $code_link $previous_links
-ln -sf -T $version_dir $code_link
-
 #start/restart app
-cd $code_link
 forever stopall
 forever start app.js
 
